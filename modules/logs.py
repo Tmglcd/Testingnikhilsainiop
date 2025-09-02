7b0770cdc8029265109a7ca161a8a2026025ea2c
Tmglcd <tmglcd06@gmail.com>
2025-09-02 23:16:27 +0530

    Add files via upload

---
+++ modules/logs.py
@@ -0,0 +1,34 @@
+# logs.py
+from pyrogram import Client
+import logging
+from logging.handlers import RotatingFileHandler
+from datetime import datetime, timedelta
+
+# Set up logging
+logging.basicConfig(
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+# Initialize logger
+logger = logging.getLogger()
+
+#========================================================================
+
+async def send_logs(client: Client, m: Message):  # Correct parameter name
+    try:
+        with open("logs.txt", "rb") as file:
+            sent = await m.reply_text("**📤 Sending you ....**")
+            await m.reply_document(document=file)
+            await sent.delete()
+    except Exception as e:
+        await m.reply_text(f"**Error sending logs:**\n<blockquote>{e}</blockquote>")
+        
+
+
